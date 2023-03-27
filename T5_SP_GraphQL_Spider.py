5b898c76154eb72bae3a5e7dea255b9f65cd9581
Jakob Tolstrup <jakobtolstrup@Jakobs-MacBook-Pro.local>
2023-03-27 11:03:07 +0200

    updated collate funciton

--- T5_SP_GraphQL_Spider.py
+++ T5_SP_GraphQL_Spider.py
@@ -668,30 +668,30 @@ class T5MultiSPModel(pl.LightningModule):
       self.train_dataset = ConcatDataset([train_dataset_g, train_dataset_s])
       self.val_dataset = ConcatDataset([val_dataset_g,val_dataset_s])
 
+  @staticmethod
   def custom_collate_fn(batch):
-    keys = batch[0].keys()
-    collated_batch = {}
+      keys = batch[0].keys()
+      collated_batch = {}
 
-    for key in keys:
-        if key in ['source_ids', 'target_ids']:
-            max_length = max([len(sample[key]) for sample in batch])
-            padded_tensors = [torch.cat([sample[key], torch.zeros(max_length - len(sample[key]), dtype=torch.long)], dim=0) for sample in batch]
-            collated_batch[key] = torch.stack(padded_tensors, dim=0)
-        else:
-            collated_batch[key] = torch.stack([sample[key] for sample in batch], dim=0)
-            print(f"Key: {key}, Data: {[sample[key] for sample in batch]}") # Add this print statement
+      for key in keys:
+          if key in ['source_ids', 'target_ids']:
+              max_length = max([len(sample[key]) for sample in batch])
+              padded_tensors = [torch.cat([sample[key], torch.zeros(max_length - len(sample[key]), dtype=torch.long)], dim=0) for sample in batch]
+              collated_batch[key] = torch.stack(padded_tensors, dim=0)
+          else:
+              collated_batch[key] = torch.stack([sample[key] for sample in batch], dim=0)
+              print(f"Key: {key}, Data: {[sample[key] for sample in batch]}") # Add this print statement
 
-    return collated_batch
-
+      return collated_batch
 
   def train_dataloader(self):
-    return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, collate_fn=partial(custom_collate_fn), num_workers=32)
+      return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, collate_fn=self.custom_collate_fn, num_workers=32)
 
   def val_dataloader(self):
-    return DataLoader(self.val_dataset, batch_size=self.batch_size, collate_fn=partial(custom_collate_fn), num_workers=32)
+      return DataLoader(self.val_dataset, batch_size=self.batch_size, collate_fn=self.custom_collate_fn, num_workers=32)
 
   def test_dataloader(self):
-    return DataLoader(self.test_dataset, batch_size=self.batch_size, collate_fn=partial(custom_collate_fn), num_workers=32)
+      return DataLoader(self.test_dataset, batch_size=self.batch_size, collate_fn=self.custom_collate_fn, num_workers=32)
 
 
 
