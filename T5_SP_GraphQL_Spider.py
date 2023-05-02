fe0296ce09c38daca5b7505be900d77ac2cb73ef
Jakob Tolstrup <jakob.l.tolstrup@gmail.com>
2023-05-02 21:03:57 +0200

    set coSQL back

--- T5_SP_GraphQL_Spider.py
+++ T5_SP_GraphQL_Spider.py
@@ -253,8 +253,8 @@ if test_state:
     print("SpiderDataset test done")
 
 class CoSQLMaskDataset(Dataset):
-    'Characterizes a dataset for PyTorch'
-    def __init__(self, tokenizer, type_path='cosql_train.json', block_size=64):
+  'Characterizes a dataset for PyTorch'
+  def __init__(self, tokenizer, type_path='cosql_train.json', block_size=64):
         'Initialization'
         super(CoSQLMaskDataset, ).__init__()
         self.tokenizer = tokenizer
@@ -262,35 +262,35 @@ class CoSQLMaskDataset(Dataset):
         self.source = []
         self.target = []
         path = './cosql_dataset/sql_state_tracking/' + type_path
-        with open(path, 'r', encoding='utf-8') as f:
-            data = json.load(f)
-            for element in data:
-                for interaction in element['interaction']:
-                    utterance = interaction['query']
-                    encoded_source = tokenizer.encode(utterance, max_length=block_size, padding='max_length', truncation=True, return_tensors='pt').squeeze()
-                    token_count = encoded_source.shape[0]
-                    repeated_utterance = [encoded_source for _ in range(token_count)]
-                    for pos in range(1, token_count):
-                        encoded_source = repeated_utterance[pos].clone()
-                        target_id = encoded_source[pos].item()
-                        if target_id == tokenizer.eos_token_id:
-                            break
+        with open(path, 'r') as f:
+          data = json.load(f)
+          for element in data:
+            for interaction in element['interaction']:
+              utterance = interaction['query']
+              encoded_source = tokenizer.encode(utterance, max_length=block_size, pad_to_max_length=True, truncation=True, return_tensors='pt').squeeze()
+              token_count = encoded_source.shape[0]
+              repeated_utterance = [encoded_source for _ in range(token_count)]
+              for pos in range(1, token_count):
+                encoded_source = repeated_utterance[pos].clone()
+                target_id = encoded_source[pos].item()
+                if target_id == tokenizer.eos_token_id:
+                  break
 
-                        encoded_source[pos] = tokenizer.mask_token_id
-                        decoded_target = ''.join(tokenizer.convert_ids_to_tokens([target_id])) + tokenizer.eos_token
-                        encoded_target = tokenizer.encode(decoded_target, return_tensors='pt', max_length=4, padding='max_length', truncation=True).squeeze()
-                        self.target.append(encoded_target)
-                        self.source.append(encoded_source)
+                encoded_source[pos] = tokenizer.mask_token_id
+                decoded_target = ''.join(tokenizer.convert_ids_to_tokens([target_id])) + ' </s>'
+                encoded_target = tokenizer.encode(decoded_target, return_tensors='pt', max_length=4, pad_to_max_length=True, truncation=True).squeeze()
+                self.target.append(encoded_target)
+                self.source.append(encoded_source)
 
-    def __len__(self):
+  def __len__(self):
         'Denotes the total number of samples'
         return len(self.source)
 
-    def __getitem__(self, index):
+  def __getitem__(self, index):
         'Generates one sample of data'
         source_ids = self.source[index]
         target_id = self.target[index]
-        return {'source_ids': source_ids,
+        return { 'source_ids': source_ids,
                 'target_id': target_id}
 
 
