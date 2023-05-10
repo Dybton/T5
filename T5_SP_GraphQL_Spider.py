303d3646e7e2c826f484fee2e3812016671765b5
Jakob Tolstrup <jakob.l.tolstrup@gmail.com>
2023-05-10 16:39:24 +0200

    synthetic_mirror_1500.json

--- T5_SP_GraphQL_Spider.py
+++ T5_SP_GraphQL_Spider.py
@@ -60,7 +60,7 @@ print("my version of pytorch_lightning is " +pytorch_lightning.__version__)
 test_state = False
 tensorflow_active = False
 dev_mode = False
-train_set = "synthetic_mirror_4500.json"
+train_set = "synthetic_mirror_1500.json"
 
 # In[3]:
 
