1a677d1a9dd679b0a432256899c3b16cd2f6acfc
Jakob Tolstrup <jakob.l.tolstrup@gmail.com>
2023-05-11 22:46:37 +0200

    ckpt 5

--- T5_SP_GraphQL_Spider.py
+++ T5_SP_GraphQL_Spider.py
@@ -768,7 +768,7 @@ trainer = pl.Trainer(logger=logger)
 # Pass the logger and checkpoint_callback to the Trainer
 trainer = pl.Trainer(callbacks=[checkpoint_callback], accelerator='gpu', max_epochs=1, log_every_n_steps=1, limit_train_batches=0.2, gpus=1)
 
-initial_training_checkpoint_path = f"checkpoints/training_checkpoint_{train_set}4.ckpt"
+initial_training_checkpoint_path = f"checkpoints/training_checkpoint_5.ckpt"
 
 if not os.path.isfile(initial_training_checkpoint_path):
     # Train the model if checkpoint does not exist
@@ -811,7 +811,7 @@ fine_tuning_checkpoint_callback = ModelCheckpoint(
 trainer = Trainer(gpus=1, max_epochs=6, progress_bar_refresh_rate=1, val_check_interval=0.5, callbacks=[fine_tuning_checkpoint_callback])
 
 
-fine_tuning_checkpoint_path = f"checkpoints/fine_tuned_checkpoint_{train_set}4.ckpt"
+fine_tuning_checkpoint_path = f"checkpoints/fine_tuned_checkpoint_{train_set}5.ckpt"
 
 
 if not os.path.isfile(fine_tuning_checkpoint_path):
