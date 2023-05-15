23cab213c7a71ac12c7762a808ecaa0fc4912b0c
Jakob Tolstrup <jakob.l.tolstrup@gmail.com>
2023-05-15 10:46:12 +0200

    mirror 3000 ckpt 1 - back to basics

--- T5_SP_GraphQL_Spider.py
+++ T5_SP_GraphQL_Spider.py
@@ -36,10 +36,7 @@ from functools import reduce
 
 import itertools
 
-# Fixing the random seeds for reproducibility, and to compare performance across differnet modifications
-torch.manual_seed(42)
-torch.cuda.manual_seed_all(42)
-random.seed(42)
+torch.manual_seed(0)
 
 import json
 from pathlib import Path
@@ -61,7 +58,7 @@ print("my version of pytorch_lightning is " + pytorch_lightning.__version__)
 test_state = False
 tensorflow_active = False
 dev_mode = False
-train_set = "synthetic_complex_4500.json"
+train_set = "synthetic_mirror_1500.json"
 
 # In[3]:
 
@@ -764,7 +761,7 @@ trainer = pl.Trainer(logger=logger)
 # Pass the logger and checkpoint_callback to the Trainer
 trainer = pl.Trainer(callbacks=[checkpoint_callback], accelerator='gpu', max_epochs=1, log_every_n_steps=1, limit_train_batches=0.2, gpus=1)
 
-initial_training_checkpoint_path = f"checkpoints/training_checkpoint_{train_set}3.ckpt"
+initial_training_checkpoint_path = f"checkpoints/training_checkpoint_{train_set}1.ckpt"
 
 if not os.path.isfile(initial_training_checkpoint_path):
     # Train the model if checkpoint does not exist
@@ -806,7 +803,7 @@ fine_tuning_checkpoint_callback = ModelCheckpoint(
 # Pass the new checkpoint_callback to the Trainer
 trainer = Trainer(gpus=1, max_epochs=6, progress_bar_refresh_rate=1, val_check_interval=0.5, callbacks=[fine_tuning_checkpoint_callback])
 
-fine_tuning_checkpoint_path = f"checkpoints/fine_tuned_checkpoint_{train_set}3.ckpt"
+fine_tuning_checkpoint_path = f"checkpoints/fine_tuned_checkpoint_{train_set}1.ckpt"
 
 if not os.path.isfile(fine_tuning_checkpoint_path):
     # Fine-tune the model if checkpoint does not exist
@@ -877,8 +874,6 @@ system.num_beams = 3
 system.task='finetune'
 system.test_flag = 'graphql'
 system.prepare_data() # We are fine tuning should come after the we have reached the testing phase.
-
-system.eval()
 
 print("Before calling trainer.test, test_dataset is:", system.test_dataset)
 trainer.test(model=system)
