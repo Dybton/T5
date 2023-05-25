ff3c7549776d62bc73836f19a2cbe6a132da73ae
Jakob Tolstrup <jakob.l.tolstrup@gmail.com>
2023-05-25 15:40:14 +0200

    complex 1500 6

--- T5_SP_GraphQL_Spider_complex.py
+++ T5_SP_GraphQL_Spider_complex.py
@@ -57,8 +57,8 @@ print("my version of pytorch_lightning is " +pytorch_lightning.__version__)
 test_state = False
 tensorflow_active = False
 dev_mode = False
-train_set = "synthetic_complex_4500.json"
-checkpoint_number = 5
+train_set = "synthetic_complex_1500.json"
+checkpoint_number = 6
 torch.manual_seed(checkpoint_number)
 
 # In[3]:
