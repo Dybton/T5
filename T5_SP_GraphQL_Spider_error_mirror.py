06421ee6bbb2133a7b1ef9e1224cc9cf467cb6b9
Jakob Tolstrup <jakob.l.tolstrup@gmail.com>
2023-05-26 22:50:25 +0200

    error mirror 1500 8

--- T5_SP_GraphQL_Spider_error_mirror.py
+++ T5_SP_GraphQL_Spider_error_mirror.py
@@ -59,7 +59,7 @@ print("my version of pytorch_lightning is " +pytorch_lightning.__version__)
 test_state = False
 tensorflow_active = False
 dev_mode = False
-train_set = "synthetic_mirror_3000.json"
+train_set = "synthetic_mirror_1500.json"
 checkpoint_number = 8
 torch.manual_seed(checkpoint_number)
 
