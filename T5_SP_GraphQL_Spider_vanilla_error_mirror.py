c6a1ba9537d7b7f4513eddf7607f90288cf34c5d
Jakob Tolstrup <jakob.l.tolstrup@gmail.com>
2023-05-26 11:15:09 +0200

    vanilla error mirror 6

--- T5_SP_GraphQL_Spider_vanilla_error_mirror.py
+++ T5_SP_GraphQL_Spider_vanilla_error_mirror.py
@@ -58,7 +58,7 @@ test_state = False
 tensorflow_active = False
 dev_mode = False
 train_set = "vanilla_error_mirror_1500.json"
-checkpoint_number = 5
+checkpoint_number = 6
 torch.manual_seed(checkpoint_number)
 
 # In[3]:
